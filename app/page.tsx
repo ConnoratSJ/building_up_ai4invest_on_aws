b2e912bb43d15be5ba3a8337b5cd8b84f1c99228
ConnoratSJ <connor.quinn@sjsu.edu>
2025-05-12 21:17:41 -0700

    going back

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,9 @@
 "use client";
-import './globals.css'; 
 
 import { useState, useEffect } from "react";
 import { generateClient } from "aws-amplify/data";
 import type { Schema } from "@/amplify/data/resource";
+import "./../app/app.css";
 import { Amplify } from "aws-amplify";
 import outputs from "@/amplify_outputs.json";
 import "@aws-amplify/ui-react/styles.css";
