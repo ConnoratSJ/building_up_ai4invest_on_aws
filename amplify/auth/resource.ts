c529b4f66fef80e4a22c73e3381755af29f933c9
ConnoratSJ <connor.quinn@sjsu.edu>
2025-05-12 19:39:15 -0700

    Update resource.ts

--- amplify/auth/resource.ts
+++ amplify/auth/resource.ts
@@ -11,21 +11,3 @@ export const auth = defineAuth({
   },
 });
 
-import { type ClientSchema, a, defineData } from '@aws-amplify/backend';
-
-const schema = a.schema({
-  Todo: a.model({
-    content: a.string(),
-  }).authorization(allow => [allow.owner()]),
-});
-
-export type Schema = ClientSchema<typeof schema>;
-
-export const data = defineData({
-  schema,
-  authorizationModes: {
-    // This tells the data client in your app (generateClient())
-    // to sign API requests with the user authentication token. 
-    defaultAuthorizationMode: 'userPool',
-  },
-});
