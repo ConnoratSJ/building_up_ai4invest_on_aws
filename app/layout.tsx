eda99a611254a4f552ac3132eacd62801a00b053
ConnoratSJ <connor.quinn@sjsu.edu>
2025-05-12 23:31:03 -0700

    removed all styling

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,7 @@
 import React from "react";
 import { Amplify } from "aws-amplify";
 import { Authenticator } from "@aws-amplify/ui-react";
-import "@aws-amplify/ui-react/styles.css";
+//import "@aws-amplify/ui-react/styles.css";
 import outputs from "@/amplify_outputs.json";
 
 Amplify.configure(outputs);
--- app/page.tsx
+++ app/page.tsx
@@ -3,10 +3,10 @@
 import { useState, useEffect } from "react";
 import { generateClient } from "aws-amplify/data";
 import type { Schema } from "@/amplify/data/resource";
-import "./../app/app.css";
+//import "./../app/app.css";
 import { Amplify } from "aws-amplify";
 import outputs from "@/amplify_outputs.json";
-import "@aws-amplify/ui-react/styles.css";
+//import "@aws-amplify/ui-react/styles.css";
 import { useAuthenticator } from "@aws-amplify/ui-react";
 
 Amplify.configure(outputs);
@@ -51,26 +51,26 @@ export default function FormPage() {
 
   return (
     <main>
-      <div className="min-h-screen bg-gray-50 font-sans">
-        <div className="flex justify-center">
-          <div className="w-full max-w-md bg-white p-6 border rounded-lg shadow-md text-black mt-16">
-            <h2 className="text-xl font-semibold text-center mb-4">Create your investment profile</h2>
+      <div>
+        <div>
+          <div>
+            <h2>Create your investment profile</h2>
 
             {step === 1 && (
               <>
                 <div>
-                  <label className="block mb-2">Username</label>
-                  <input name="username" value={formData.username} onChange={handleChange} className="w-full mb-4 p-2 border rounded" />
+                  <label>Username</label>
+                  <input name="username" value={formData.username} onChange={handleChange}/>
                 </div>
-                <button onClick={() => setStep(2)} className="w-full bg-gray-700 text-white py-2 rounded hover:bg-black">Next</button>
+                <button onClick={() => setStep(2)}>Next</button>
               </>
             )}
 
           {step === 2 && (
             <>
               <div>
-                <label className="block mb-2">Age Group</label>
-                <select name="Age Group" value={formData["Age Group"]} onChange={handleChange} className="w-full mb-4 p-2 border rounded">
+                <label >Age Group</label>
+                <select name="Age Group" value={formData["Age Group"]} onChange={handleChange} >
                   <option value="">-- Please select an option --</option>
                   <option value="1">18-24</option>
                   <option value="2">25-34</option>
@@ -79,14 +79,14 @@ export default function FormPage() {
                   <option value="5">55-64</option>
                   <option value="6">65+</option>
                 </select>
-                <label className="block mb-2">Ethnicity</label>
-                <select name="Ethnicity" value={formData["Ethnicity"]} onChange={handleChange} className="w-full mb-4 p-2 border rounded">
+                <label >Ethnicity</label>
+                <select name="Ethnicity" value={formData["Ethnicity"]} onChange={handleChange} >
                   <option value="">-- Please select an option --</option>
                   <option value="1">White non-Hispanic</option>
                   <option value="2">Non-White</option>
                 </select>              
-                <label className="block mb-2">Highest level of education completed</label>
-                <select name="Education Level" value={formData["Education Level"]} onChange={handleChange} className="w-full mb-4 p-2 border rounded">
+                <label >Highest level of education completed</label>
+                <select name="Education Level" value={formData["Education Level"]} onChange={handleChange} >
                   <option value="">-- Please select an option --</option>
                   <option value="1">Did not complete High school</option>
                   <option value="2">High school diploma</option>
@@ -96,8 +96,8 @@ export default function FormPage() {
                   <option value="6">Bachelor's degree</option>
                   <option value="7">Post graduate degree</option>
                 </select>              
-                <label className="block mb-2">Marital Status</label>
-                <select name="Marital Status" value={formData["Marital Status"]} onChange={handleChange} className="w-full mb-4 p-2 border rounded">
+                <label >Marital Status</label>
+                <select name="Marital Status" value={formData["Marital Status"]} onChange={handleChange} >
                   <option value="">-- Please select an option --</option>
                   <option value="1">Married</option>
                   <option value="2">Single</option>
@@ -106,8 +106,8 @@ export default function FormPage() {
                   <option value="5">Widowed/widower</option>
                   <option value="99">Prefer not to say</option>
                 </select>   
-                <label className="block mb-2">Number of financially dependent children</label>
-                <select name="Financially dependent children" value={formData["Financially dependent children"]} onChange={handleChange} className="w-full mb-4 p-2 border rounded">
+                <label >Number of financially dependent children</label>
+                <select name="Financially dependent children" value={formData["Financially dependent children"]} onChange={handleChange} >
                   <option value="">-- Please select an option --</option>
                   <option value="1">1</option>
                   <option value="2">2</option>
@@ -118,21 +118,19 @@ export default function FormPage() {
                   <option value="99">Prefer not to say</option>
                 </select>  
             </div>
-            <button onClick={() => setStep(2)} className="w-full bg-gray-700 text-white py-2 rounded hover:bg-black">Back</button>
   
-            <button onClick={() => setStep(3)} className="w-full bg-gray-700 text-white py-2 rounded hover:bg-black">Next</button>
+            <button onClick={() => setStep(3)} >Next</button>
 
             </>
           )}
           {step === 3 && (
             <>
             <div>
-              <label className="block mb-2">Approximate annual household income</label>
+              <label >Approximate annual household income</label>
             <select
                 name="Annual Household Income"
                 value={formData["Annual Household Income"]}
                 onChange={handleChange}
-                className="w-full mb-4 p-2 border rounded"
                 >
                 <option value="">-- Please select an option --</option>
                 <option value="1">Less than $15,000</option>
@@ -148,12 +146,11 @@ export default function FormPage() {
                 <option value="98">Don't know</option>
                 <option value="99">Prefer not to say</option>
             </select>
-            <label className="block mb-2">Over the past year, spending relative to income</label>
+            <label >Over the past year, spending relative to income</label>
             <select
                 name="spending_vs_income"
                 value={formData.spending_vs_income}
                 onChange={handleChange}
-                className="w-full mb-4 p-2 border rounded"
                 >
                 <option value="">-- Please select an option --</option>
                 <option value="1">Spending less than income</option>
@@ -162,12 +159,11 @@ export default function FormPage() {
                 <option value="98">Don't know</option>
                 <option value="99">Prefer not to say</option>
             </select>              
-            <label className="block mb-2">In a typical month, how difficult to cover expenses/pay bills</label>
+            <label >In a typical month, how difficult to cover expenses/pay bills</label>
             <select
                 name="difficulty_covering_expenses"
                 value={formData.difficulty_covering_expenses}
                 onChange={handleChange}
-                className="w-full mb-4 p-2 border rounded"
                 >
                 <option value="">-- Please select an option --</option>
                 <option value="1">Very difficult</option>
@@ -176,12 +172,11 @@ export default function FormPage() {
                 <option value="98">Don't know</option>
                 <option value="99">Prefer not to say</option>
             </select>              
-            <label className="block mb-2">Have set aside emergency funds to cover 3 months’ expenses</label>
+            <label >Have set aside emergency funds to cover 3 months’ expenses</label>
             <select
                 name="Emergency fund to cover 3 Months expenses"
                 value={formData["Emergency fund to cover 3 Months expenses"]}
                 onChange={handleChange}
-                className="w-full mb-4 p-2 border rounded"
                 >
                 <option value="">-- Please select an option --</option>
                 <option value="1">Yes</option>
@@ -189,12 +184,11 @@ export default function FormPage() {
                 <option value="98">Don't know</option>
                 <option value="99">Prefer not to say</option>
             </select>   
-            <label className="block mb-2">Willingness to take financial risks when investing</label>
+            <label >Willingness to take financial risks when investing</label>
             <select
                 name="risk_tolerance"
                 value={formData.risk_tolerance}
                 onChange={handleChange}
-                className="w-full mb-4 p-2 border rounded"
                 >
                 <option value="">-- Please select an option --</option>
                 <option value="1">1 - Not At All Willing</option>
@@ -211,19 +205,17 @@ export default function FormPage() {
                 <option value="99">Prefer not to say</option>
               </select>
               </div>
-              <button onClick={() => setStep(3)} className="w-full bg-gray-700 text-white py-2 rounded hover:bg-black">Back</button>
-              <button onClick={() => setStep(4)} className="w-full bg-gray-700 text-white py-2 rounded hover:bg-black">Next</button>
+              <button onClick={() => setStep(4)} >Next</button>
             </>
           )}  
           {step === 4 && (
             <>
             <div>
-              <label className="block mb-2">Do you have a checking account?</label>
+              <label >Do you have a checking account?</label>
             <select
                 name="Account ownership check"
                 value={formData["Account ownership check"]}
                 onChange={handleChange}
-                className="w-full mb-4 p-2 border rounded"
                 >
                 <option value="">-- Please select an option --</option>
                 <option value="1">Yes</option>
@@ -231,12 +223,11 @@ export default function FormPage() {
                 <option value="98">Don't know</option>
                 <option value="99">Prefer not to say</option>
             </select>
-            <label className="block mb-2">Do you have a savings account, money market account, or CDs?</label>
+            <label >Do you have a savings account, money market account, or CDs?</label>
             <select
                 name="Savings/Money market/CD account ownership"
                 value={formData["Savings/Money market/CD account ownership"]}
                 onChange={handleChange}
-                className="w-full mb-4 p-2 border rounded"
                 >
                 <option value="">-- Please select an option --</option>
                 <option value="1">Yes</option>
@@ -244,12 +235,11 @@ export default function FormPage() {
                 <option value="98">Don't know</option>
                 <option value="99">Prefer not to say</option>
             </select>              
-            <label className="block mb-2">Have any employer-sponsored retirement plan (401(k), pension, etc.)?</label>
+            <label >Have any employer-sponsored retirement plan (401(k), pension, etc.)?</label>
             <select
                 name="Employer-sponsored retirement plan ownership"
                 value={formData["Employer-sponsored retirement plan ownership"]}
                 onChange={handleChange}
-                className="w-full mb-4 p-2 border rounded"
                 >
                 <option value="">-- Please select an option --</option>
                 <option value="1">Yes</option>
@@ -257,12 +247,11 @@ export default function FormPage() {
                 <option value="98">Don't know</option>
                 <option value="99">Prefer not to say</option>
             </select>   
-            <label className="block mb-2">Do you regularly contribute to a retirement account (401(k), IRA, etc.)?</label>
+            <label >Do you regularly contribute to a retirement account (401(k), IRA, etc.)?</label>
             <select
                 name="Regular contribution to a retirement account"
                 value={formData["Regular contribution to a retirement account"]}
                 onChange={handleChange}
-                className="w-full mb-4 p-2 border rounded"
                 >
                 <option value="">-- Please select an option --</option>
                 <option value="1">Yes</option>
@@ -270,12 +259,11 @@ export default function FormPage() {
                 <option value="98">Don't know</option>
                 <option value="99">Prefer not to say</option>
             </select>  
-            <label className="block mb-2">Not including retirement accounts, do you have investments in stocks/bonds/mutual funds?</label>
+            <label >Not including retirement accounts, do you have investments in stocks/bonds/mutual funds?</label>
             <select
                 name="Non-retirement investments in stocks, bonds, mutual funds"
                 value={formData["Non-retirement investments in stocks, bonds, mutual funds"]}
                 onChange={handleChange}
-                className="w-full mb-4 p-2 border rounded"
                 >
                 <option value="">-- Please select an option --</option>
                 <option value="1">Yes</option>
@@ -283,12 +271,11 @@ export default function FormPage() {
                 <option value="98">Don't know</option>
                 <option value="99">Prefer not to say</option>
             </select>             
-            <label className="block mb-2">Do you [or your spouse/partner] currently own your home?</label>
+            <label >Do you [or your spouse/partner] currently own your home?</label>
             <select
                 name="Homeownership"
                 value={formData["Homeownership"]}
                 onChange={handleChange}
-                className="w-full mb-4 p-2 border rounded"
                 >
                 <option value="">-- Please select an option --</option>
                 <option value="1">Yes</option>
@@ -297,20 +284,18 @@ export default function FormPage() {
                 <option value="99">Prefer not to say</option>
             </select>
             </div>   
-              <button onClick={() => setStep(4)} className="w-full bg-gray-700 text-white py-2 rounded hover:bg-black">Back</button>
 
-              <button onClick={() => setStep(5)} className="w-full bg-gray-700 text-white py-2 rounded hover:bg-black">Next</button>
+              <button onClick={() => setStep(5)} >Next</button>
             </>
           )}  
           {step === 5 && (
             <>
             <div>
-              <label className="block mb-2">Satisfaction with current personal financial condition</label>
+              <label >Satisfaction with current personal financial condition</label>
             <select
                 name="Current financial condition satisfaction"
                 value={formData["Current financial condition satisfaction"]}
                 onChange={handleChange}
-                className="w-full mb-4 p-2 border rounded"
                 >
                 <option value="">-- Please select an option --</option>
                 <option value="1">1 - Not At All Satisfied</option>
@@ -326,12 +311,11 @@ export default function FormPage() {
                 <option value="98">Don't know</option>
                 <option value="99">Prefer not to say</option>
             </select>
-            <label className="block mb-2">Frequency of thinking about personal financial condition</label>
+            <label >Frequency of thinking about personal financial condition</label>
             <select
                 name="Thinking about FC frequency"
                 value={formData["Thinking about FC frequency"]}
                 onChange={handleChange}
-                className="w-full mb-4 p-2 border rounded"
                 >
                 <option value="">-- Please select an option --</option>
                 <option value="1">Never</option>
@@ -343,12 +327,11 @@ export default function FormPage() {
                 <option value="98">Don't know</option>
                 <option value="99">Prefer not to say</option>
             </select>              
-            <label className="block mb-2">“I am good at dealing with day-to-day financial matters”</label>
+            <label >“I am good at dealing with day-to-day financial matters”</label>
             <select
                 name="Self-efficacy"
                 value={formData["Self-efficacy"]}
                 onChange={handleChange}
-                className="w-full mb-4 p-2 border rounded"
                 >
                 <option value="">-- Please select an option --</option>
                 <option value="1">1 - Strongly Disagree</option>
@@ -361,12 +344,11 @@ export default function FormPage() {
                 <option value="98">Don't know</option>
                 <option value="99">Prefer not to say</option>
             </select>              
-            <label className="block mb-2">On a scale from 1 to 7, where 1 means very low and 7 means very high, how would you assess your overall financial knowledge?</label>
+            <label >On a scale from 1 to 7, where 1 means very low and 7 means very high, how would you assess your overall financial knowledge?</label>
             <select
                 name="Self-rated overall financial knowledge"
                 value={formData["Self-rated overall financial knowledge"]}
                 onChange={handleChange}
-                className="w-full mb-4 p-2 border rounded"
                 >
                 <option value="">-- Please select an option --</option>
                 <option value="1">1 - Very Low</option>
@@ -380,9 +362,8 @@ export default function FormPage() {
                 <option value="99">Prefer not to say</option>
             </select>  
             </div> 
-            <button onClick={() => setStep(5)} className="w-full bg-gray-700 text-white py-2 rounded hover:bg-black">Back</button>
 
-            <button /*onClick={() => window.location.href = '/dashboard'}/*onClick={handleSubmit}*/className="w-full bg-green-600 text-white py-2 rounded hover:bg-green-700">
+            <button /*onClick={() => window.location.href = '/dashboard'}/*onClick={handleSubmit}*/>
                 Submit
             </button>
               </>
